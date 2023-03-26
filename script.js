82f1c8127675a5df90b70e67c4f52af79765fea0
Uminder1313 <umindersingh56@gmail.com>
2023-03-26 11:21:23 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// let passwordInput = document.getElementById('txtPassword'),
+//     toggle = document.getElementById('btnToggle'),
+//     icon = document.getElementById("eyeIcon");
+
+
+//     function togglePassword(){
+//         if(passwordInput.type === 'password'){
+//             passwordInput.type= 'text';
+//             icon.classList.add("fa-eye-slash");
+//         } else{
+//             passwordInput.type = 'password';
+//             icon.classList.remove("fa-eye-slash")
+//         }
+//     }
+
+//     function checkInput (){
+
+//     }
+
+//     toggle.addEventListener('click', togglePassword, false);
+//     passwordInput.addEventListener('keyup', checkInput, false);
